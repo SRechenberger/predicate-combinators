74ce92fdcde596a1cea29308ea1f6d8d468fec08
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2015-11-23 14:33:50 +0100

    initial commit

---
+++ src/Data/Predicate.hs
@@ -0,0 +1,26 @@
+module Data.Function.Predicate 
+( et
+, vel
+, non
+, est
+, nonEst
+, quasi )
+where
+
+et :: (a -> Bool) -> (a -> Bool) -> a -> Bool 
+p1 `et` p2 = \x -> p1 x && p2 x 
+
+vel :: (a -> Bool) -> (a -> Bool) -> a -> Bool 
+p1 `vel` p2 = \x -> p1 x || p2 x 
+
+non :: (a -> Bool) -> a -> Bool 
+non p = not . p 
+
+est :: (a -> b) -> (b -> Bool) -> a -> Bool
+est f p = p . f
+
+nonEst :: (a -> b) -> (b -> Bool) -> a -> Bool
+nonEst f p = not . est f p
+
+quasi :: Eq b => (a -> b) -> b -> a -> Bool
+quasi f x = f `est` (== x) 
